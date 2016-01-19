354d53e7dc6979e859d9939ca5bcbff3a996744f
David Kudera <kudera.d@gmail.com>
2016-01-19 13:56:40 +0100

    Add missing constants for PHP7

--- src/constants.php
+++ src/constants.php
@@ -23,3 +23,11 @@ if (!defined('T_POW_EQUAL')) {
 if (!defined('T_SPACESHIP')) {
 	define('T_SPACESHIP', 10205);
 }
+
+if (!defined('T_CHARACTER')) {
+	define('T_CHARACTER', 10206);
+}
+
+if (!defined('T_BAD_CHARACTER')) {
+	define('T_BAD_CHARACTER', 10207);
+}
